8cef0af272ebd0fea6b9d8a2010c7f41992fb101
Alex77784 <7509608@gmail.com>
2023-07-19 18:19:07 +0300

    done ready-companies appearing-question

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // burger
 burgerIcon.addEventListener('click', () => { toggleClass([burgerIcon, headerInner, bodyLock]) });
+
 navLinks.forEach(link => link.addEventListener('click', () => {
     removeClass([burgerIcon, headerInner, bodyLock]);
 }));
